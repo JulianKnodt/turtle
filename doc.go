30f112120c97a8d3e8fc118798b77af5eebc3e35
julianknodt <julianknodt@gmail.com>
2018-08-21 01:31:17 -0700

    Added some docs

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Turtle wraps a simple drawing
+// API for a pen on a page
+//
+// It's like the old basic one.
+package turtle
--- turtle.go
+++ turtle.go
@@ -8,11 +8,22 @@ import (
 	"os"
 )
 
+// TurtleDrawer represents a turtle that scoots around the page
+// and has a pen for drawing
 type TurtleDrawer interface {
+  // Move some distance with the pen on the page
 	Move(float64)
+
+  // Rotate the turtle some radians
 	Rotate(float64)
+
+  // Partition one turtle into many turtles
 	Partition(float64) *TurtleFleet
+
+  // Len is how many turtles are in this drawer
 	Len() int
+
+  // Save turtle image into a file
 	Save(filename string) error
 }
 
@@ -28,14 +39,17 @@ type Turtle struct {
 	Image *image.RGBA
 }
 
+// Create a new image for this turtle to draw on
 func (t *Turtle) NewImage(height, width int) {
 	t.Image = image.NewRGBA(image.Rect(0, 0, width, height))
 }
 
+// Len of one turtle is always one
 func (t Turtle) Len() int {
 	return 1
 }
 
+// Move this turtle straight ahead some distance
 func (t *Turtle) Move(dist float64) {
 	for i := 0.0; i < dist; i++ {
 		t.X += t.XDir
@@ -50,6 +64,7 @@ func (t *Turtle) Move(dist float64) {
 	}
 }
 
+// Rotate this turtle some radians counterclockwise
 func (t *Turtle) Rotate(theta float64) {
 	cosTheta := math.Cos(theta)
 	sinTheta := math.Sin(theta)
@@ -58,12 +73,16 @@ func (t *Turtle) Rotate(theta float64) {
 	t.YDir = currXDir*sinTheta + t.YDir*cosTheta
 }
 
+// Make a new turtle through the power of science
 func (t *Turtle) Clone() *Turtle {
 	result := new(Turtle)
 	*result = *t
 	return result
 }
 
+// Make one turtle into two and call it a fleet
+//
+// The two get rotated by +theta and -theta
 func (t *Turtle) Partition(theta float64) *TurtleFleet {
 	clone1 := t.Clone()
 	clone2 := t.Clone()
@@ -74,10 +93,12 @@ func (t *Turtle) Partition(theta float64) *TurtleFleet {
 	}
 }
 
+// Convenient function to turn degrees into rads
 func DegreesToRadians(deg float64) float64 {
 	return deg * math.Pi / 180
 }
 
+// Save your turtle to a file
 func (t Turtle) Save(filename string) error {
 	f, err := os.Create(filename)
 	if err != nil {
